1424e8dc22012a1ec3fa3e1c709dbcab2dffb79a
Olufunto Oyewole <funti009@gmail.com>
2022-06-09 02:52:16 +0100

    made function for bird falling down

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
+import {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Bird from './components/Bird';
 
 export default function App() {
+    const screenWidth=Dimensions.get('screen').width;
+    const screenHeight=Dimensions.get('screen').height;
+    const birdLeft= screenWidth/2;
+    const [BirdBottom, setBirdBottom]= useState(screenHeight/2); 
+    const gravity=3;
+    let gameTimerId;
+    
+    useEffect(()=>{
+        if(BirdBottom>0){
+            gameTimerId=setInterval(()=>{
+                setBirdBottom(BirdBottom>BirdBottom-gravity)
+            },30)
+        }
+        
+        return()=>{
+            clearInterval(gameTimerId)
+        }
+    },[BirdBottom])
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Bird />
     </View>
   );
 }
